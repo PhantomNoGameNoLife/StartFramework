1e9f7e12308c57242776d6d3734e969c5611dbd0
PhantomNoGameNoLife <phantomnogamenolife@gmail.com>
2025-07-29 02:31:47 +0300

    up

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -27,14 +27,14 @@ const Contact = () => {
             </div>
             <form action="" className='p-3 w-100'>
               <label className={isTop.user ? Style.top : ''} htmlFor="username">userName:</label>
-              <input id='username' className="form-control py-3 border-0 border-bottom w-100" type="text" onChange={(e) => labelTop('user', e)} placeholder="userName" aria-label="User Name"></input>
+              <input id='username' className="form-control py-3 border-0 border-bottom w-100" type="text" onChange={(e) => labelTop('user', e)} placeholder="userName" aria-label="User Name" autoComplete='name'></input>
               <label className={isTop.age ? Style.top : ''} htmlFor="userage">userAge:</label>
               <input id='userage' className="form-control py-3 border-0 border-bottom w-100" type="number" onChange={(e) => labelTop('age', e)} placeholder="userage" aria-label="User Age" min={16} max={100}></input>
               <label className={isTop.email ? Style.top : ''} htmlFor="useremail">userEmail:</label>
-              <input id='useremail' className="form-control py-3 border-0 border-bottom w-100" type="email" onChange={(e) => labelTop('email', e)} placeholder="useremail" aria-label="User Email"></input>
+              <input id='useremail' className="form-control py-3 border-0 border-bottom w-100" type="email" onChange={(e) => labelTop('email', e)} placeholder="useremail" aria-label="User Email" autoComplete='email'></input>
               <label className={isTop.pass ? Style.top : ''} htmlFor="userpassword">userPassword:</label>
               <input id='userpassword' className="form-control py-3 border-0 border-bottom w-100" type="password" onChange={(e) => labelTop('pass', e)} placeholder="userpassword" aria-label="User Password" minLength={6}></input>
-              <button class={`btn mt-4 text-white ${Style.bg}`}>Send Message</button>
+              <button className={`btn mt-4 text-white ${Style.bg}`}>Send Message</button>
             </form>
           </div>
         </div>
