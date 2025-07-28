d354e69799e61130c4540656cbbb94edcc29a299
PhantomNoGameNoLife <phantomnogamenolife@gmail.com>
2025-07-29 02:22:43 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ function App() {
       {
         path: '', element: <Layout />, children: [
           { index: true, element: <Home /> },
-          { path: '/about', element: <About /> },
-          { path: '/portfolio', element: <Portfolio /> },
-          { path: '/contact', element: <Contact /> },
+          { path: 'about', element: <About /> },
+          { path: 'portfolio', element: <Portfolio /> },
+          { path: 'contact', element: <Contact /> },
           { path: '*', element: <NotFound /> },
         ]
       }
